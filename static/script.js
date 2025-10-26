429560623b48b8d69c5cb8cf92721586bf31c97f
jukramer <justas.kramer@gmail.com>
2025-10-26 18:48:10 +0100

    form updates

--- static/script.js
+++ static/script.js
@@ -1,5 +1,18 @@
 const form = document.getElementById('myForm')
+const fundCheckbox = document.getElementById('fund-requested')
+const fundBudget = document.getElementById('fund-budget-block')
 
+// Hide/unhide fund amount
+fundCheckbox.addEventListener('change', () => {
+    if (fundCheckbox.checked) {
+        fundBudget.style.display = 'inline-block';
+    } else {
+        fundBudget.style.display = 'none';
+    }
+});
+
+
+// Communication with Flask
 form.addEventListener('submit', async (event) => {
     event.preventDefault();
 
